183d73acdd2e6581c0cd50b727d0fb1718798e79
jd <johnnytriple33@gmail.com>
2023-01-26 10:51:22 -0800

    Add RegistrationController

---
+++ thymeleafformdemo/src/main/java/com/example/thymeleafformdemo/RegistrationController.java
@@ -0,0 +1,39 @@
+package com.example.thymeleafformdemo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @Autowired
+    EphemeralPeristanceStore peristanceStore;
+
+    @GetMapping()
+    public String registrationForm(Model model) {
+        model.addAttribute("mylogin", new LoginPOJO());
+        return "register";
+    }
+
+    @PostMapping()
+    public String saveRegistrationSubmission(Model model, LoginPOJO mylogin) {
+        System.out.println(">>>>>> saveRegistrationSubmission");
+        System.out.println(">>>>>> uname="+mylogin.getUname());
+        System.out.println(">>>>>> pass ="+mylogin.getPassword());
+        // TODO:
+        model.addAttribute("mylogin", mylogin);
+        if(peristanceStore.store(mylogin)) {
+            return "login-result";
+        } else {
+            return "registration-error";
+        }
+    }
+
+}
